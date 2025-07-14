8d987dc2e6d1214b9b6761be64364f67a7bfb32a
TanmayPatil28 <Tp@628271@gmail.com>
2025-07-14 21:29:18 +0530

    chore: Update server configuration for improved performance and security

--- server/src/index.ts
+++ server/src/index.ts
@@ -11,7 +11,7 @@ import apiRoutes from './routes/index'
 dotenv.config()
 
 const app = express()
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 6060
 
 // Middleware
 app.use(helmet())

61c792c842bb79f27c80692da68c79355581927c
TanmayPatil28 <Tp@628271@gmail.com>
2025-07-14 20:32:00 +0530

    fix: Remove file extension from App import statement for consistency

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import { BrowserRouter } from 'react-router-dom'
 import { Toaster } from 'react-hot-toast'
-import App from './App.tsx'
+import App from './App'
 import AppWrapper from './AppWrapper.tsx'
 import './index.css'
 
